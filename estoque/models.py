e3c6f4eb239667acd6304978ce1e4dda201aa2f7
Marcos Felinto <marcos.leao@escolar.ifrn.edu.br>
2023-04-17 17:03:40 -0300

    nova classe

--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50, verbose_name="Nome")
+    preco = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="Preço")
+    descricao = models.CharField(max_length=200, verbose_name="Descrição")
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
