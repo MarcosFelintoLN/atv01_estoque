617f9f624fcd99a8ea6b4a0dcee232cec3010976
Marcos Felinto <marcos.leao@escolar.ifrn.edu.br>
2023-04-20 17:16:09 -0300

    nova funcao

--- estoque/urls.py
+++ estoque/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.listar,name='index.html'),
     path('cadastrar/',views.cadastrar,name='cadastrar_produto.html'),
     path('excluir/<int:pk>/', views.excluir, name='excluir'),
+    path('editar/<int:pk>/', views.editar, name='editar')
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -25,3 +25,20 @@ def excluir(request, pk):
     produto_escolhido = Produto.objects.get(pk=pk)
     produto_escolhido.delete()
     return redirect('index.html')
+
+def editar(request, pk):
+    template_name = "estoque/cadastrar_produto.html"
+    editar_produto = Produto.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=editar_produto)
+        if form.is_valid():
+            editar_produto = form.save()
+            editar_produto.nome = form.data['nome']
+            editar_produto.preco = form.data['preco']
+            editar_produto.descricao = form.data['descricao']
+            editar_produto.quantidade = form.data['quantidade']
+            editar_produto.save()
+            return redirect('index.html')
+    else:
+        form = ProdutoForm(instance=editar_produto)
+    return render(request,template_name,{'form':form})
